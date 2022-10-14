05581d6391cba07400a4752ffb21b18fcb2d4206
Inanna Malick <inanna@recursion.wtf>
2022-10-14 10:33:36 -0700

    code golf main method

--- src/lib.rs
+++ src/lib.rs
@@ -15,32 +15,27 @@ pub fn parse_and_run<F: FnMut(String)>(
 ) -> Result<(), anyhow::Error> {
     use walkdir::WalkDir;
 
-    let e: Result<
-        (
-            expr::ExprTree,
-            position::Stream<&str, position::SourcePosition>,
-        ),
-        combine::easy::Errors<_, _, _>,
-    > = combine::EasyParser::easy_parse(&mut parser::or(), position::Stream::new(&s[..]));
+    match combine::EasyParser::easy_parse(&mut parser::or(), position::Stream::new(&s[..]))
+    {
+        Ok((e, _)) => {
+            println!("running with expression: {:?}", e);
 
-    let e = match e {
-        Ok(e) => e.0,
-        Err(err) => panic!("parse error: {}", err),
-    };
-
-    println!("running with expression: {:?}", e);
-
-    let walker = WalkDir::new(root).into_iter();
-    for entry in walker {
-        let entry = entry?;
-        if !entry.metadata()?.is_dir() {
-            // hacky, will panic sometimes if bad OsStr (FIXME)
-            let path = entry.path().to_str().unwrap().to_owned();
-            if eval(&e, entry)? {
-                on_match(path);
+            let walker = WalkDir::new(root).into_iter();
+            for entry in walker {
+                let entry = entry?;
+                if !entry.metadata()?.is_dir() {
+                    // hacky, will panic sometimes if bad OsStr (FIXME)
+                    let path = entry.path().to_str().unwrap().to_owned();
+                    if eval(&e, entry)? {
+                        on_match(path);
+                    }
+                }
             }
+
+
+                    Ok(())
         }
+        Err(err) => panic!("parse error: {}", err),
     }
 
-    Ok(())
 }
