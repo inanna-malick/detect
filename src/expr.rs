7e9dd9167824c7d3a3183937a7c32cfaa2620e60
Inanna Malick <inanna@recursion.wtf>
2023-10-18 23:22:48 -0700

    cleanup

--- src/expr.rs
+++ src/expr.rs
@@ -1,12 +1,10 @@
 pub mod frame;
 pub mod short_circuit;
 
-use std::fmt::Display;
-
+use self::short_circuit::ShortCircuit;
 use crate::expr::frame::ExprFrame;
 pub(crate) use crate::predicate::{ContentPredicate, MetadataPredicate, NamePredicate};
 use recursion::CollapsibleExt;
-use self::short_circuit::ShortCircuit;
 
 /// Filesystem entity matcher expression with boolean logic and predicates
 pub enum Expr<Predicate> {
@@ -20,23 +18,7 @@ pub enum Expr<Predicate> {
     Literal(bool),
 }
 
-impl<A: Display> Display for Expr<A> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Not(a) => write!(f, "!{}", a),
-            Self::And(a, b) => {
-                write!(f, "{} && {}", a, b)
-            }
-            Self::Or(a, b) => {
-                write!(f, "{} || {}", a, b)
-            }
-            Self::Predicate(arg0) => write!(f, "{}", arg0),
-            Self::Literal(arg0) => write!(f, "{}", arg0),
-        }
-    }
-}
-
-impl<A: Display + Clone> Expr<A> {
+impl<A: Clone> Expr<A> {
     pub fn and(a: Self, b: Self) -> Self {
         Self::And(Box::new(a), Box::new(b))
     }
@@ -47,7 +29,7 @@ impl<A: Display + Clone> Expr<A> {
         Self::Not(Box::new(a))
     }
 
-    pub fn reduce_predicate_and_short_circuit<B: Display + Clone>(
+    pub fn reduce_predicate_and_short_circuit<B: Clone>(
         &self,
         f: impl Fn(A) -> ShortCircuit<B>,
     ) -> Expr<B> {
@@ -76,5 +58,4 @@ impl<A: Display + Clone> Expr<A> {
             ExprFrame::Literal(x) => Expr::Literal(x),
         })
     }
-
 }
