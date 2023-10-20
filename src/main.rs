e74dcfb316536ada695f63752ccda3e6003bf700
Inanna Malick <inanna@recursion.wtf>
2023-10-19 21:35:29 -0700

    add docs and make expr positional arg

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use clap::Parser;
+use clap::{Parser, command};
 use detect::parse_and_run;
 
+
+/// operators
+/// - `a && b` a and b
+/// - `a || b`: a or b
+/// - `!a`: not a
+/// - `(a)`: parens to clarify grouping
+/// 
+/// file path predicates
+/// - `filename($REGEX)`: file name
+/// - `filepath($REGEX)`: file path
+/// - `extension($STRING)` exact match on extension
+/// 
+/// metadata predicates
+/// - `dir()`: is dir
+/// - `executable()`: is executable
+/// - `size(n1..n2)`: file size in range n1 to n2
+/// - `size(..n)`: file size less than n
+/// - `size(n..)`: file size larger than n
+/// 
+/// file contents predicates
+/// - `contains($REGEX)`: file contents
+/// - `utf8()`: file contents are utf8
+/// 
+/// for example:
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[command(name = "detect", author, version, about, long_about, verbatim_doc_comment)]
 struct Args {
     /// filtering expr
-    #[clap(short, long)]
+    #[clap(index=1)]
     expr: String,
 }
 
