a55b99e83a23cf6cb7bede8c38ef37d5b3126082
Inanna Malick <inanna@recursion.wtf>
2024-07-21 16:10:03 -0700

    working error printer

--- src/parser2.rs
+++ src/parser2.rs
@@ -1,13 +1,10 @@
 use nom::character::complete::*;
 use nom::combinator::{all_consuming, cut, map_res};
-use nom::error::{Error, ParseError, VerboseError};
-use nom::multi::separated_list1;
-use nom::sequence::delimited;
+use nom::error::{VerboseError, VerboseErrorKind};
+use nom::Parser;
 use nom::{branch::*, bytes::complete::tag};
-use nom::{IResult, Parser};
 use nom_locate::LocatedSpan;
 use nom_recursive::{recursive_parser, RecursiveInfo};
-use regex::escape;
 
 use crate::expr::{ContentPredicate, Expr, MetadataPredicate, NamePredicate};
 use crate::predicate::{NumericalOp, Op, Predicate, RawPredicate, Selector};
@@ -17,7 +14,7 @@ use crate::predicate::{NumericalOp, Op, Predicate, RawPredicate, Selector};
 type Span<'a> = LocatedSpan<&'a str, RecursiveInfo>;
 
 // type IResult<A, B> = IResult<A, B, VerboseError<A>>;
-// pub type IResult<I, O, E = VerboseError<I>> = Result<(I, O), nom::Err<E>>;
+pub type IResult<I, O, E = VerboseError<I>> = Result<(I, O), nom::Err<E>>;
 
 pub fn expr(
     s: Span,
@@ -134,22 +131,135 @@ fn operator(s: Span) -> IResult<Span, Op> {
 
 #[test]
 fn test() {
-    // let mut or = separated_list1(ws(tag::<&str, &str, Error<_>>("||")), alpha0);
+    let data = "@name ~= test || @size == 5 && (@name == test || @name == test)";
 
-    // let ret = x("a || b || c && d || e");
-    // println!("{:?}", ret.unwrap());
+    let data: LocatedSpan<&str, RecursiveInfo> = LocatedSpan::new_extra(data, RecursiveInfo::new());
 
-    let ret = expr(LocatedSpan::new_extra(
-        "@name ~= test || @size == 5 && (@name == test || @name == test)",
-        RecursiveInfo::new(),
-    ));
+    let ret = expr(data);
 
-    println!("{:?}", ret.unwrap().1);
-    // if let nom::Err::Failure(errs) = ret.err().unwrap() {
-    //     for e in errs.errors.iter() {
+    match ret {
+        Err(nom::Err::Error(e)) | Err(nom::Err::Failure(e)) => {
+            println!(
+                "verbose errors - `root::<VerboseError>(data)`:\n{}",
+                convert_error2(data, e)
+            );
+        }
+        Ok(x) => {
+            println!("{:?}", x.1);
+        }
+        _ => {}
+    }
+}
 
-    //         println!("{:?}", e);
-    //     }
+// copy of nom's convert_error specialized to nom_locate span type, fixes type error on Deref
+pub fn convert_error2(input: Span, e: VerboseError<Span>) -> nom::lib::std::string::String {
+    use nom::lib::std::fmt::Write;
+    use nom::Offset;
 
-    // }
+    let mut result = nom::lib::std::string::String::new();
+
+    for (i, (substring, kind)) in e.errors.iter().enumerate() {
+        let offset = input.offset(substring);
+
+        if input.is_empty() {
+            match kind {
+                VerboseErrorKind::Char(c) => {
+                    write!(&mut result, "{}: expected '{}', got empty input\n\n", i, c)
+                }
+                VerboseErrorKind::Context(s) => {
+                    write!(&mut result, "{}: in {}, got empty input\n\n", i, s)
+                }
+                VerboseErrorKind::Nom(e) => {
+                    write!(&mut result, "{}: in {:?}, got empty input\n\n", i, e)
+                }
+            }
+        } else {
+            let prefix = &input.as_bytes()[..offset];
+
+            // Count the number of newlines in the first `offset` bytes of input
+            let line_number = prefix.iter().filter(|&&b| b == b'\n').count() + 1;
+
+            // Find the line that includes the subslice:
+            // Find the *last* newline before the substring starts
+            let line_begin = prefix
+                .iter()
+                .rev()
+                .position(|&b| b == b'\n')
+                .map(|pos| offset - pos)
+                .unwrap_or(0);
+
+            // Find the full line after that newline
+            let line = input[line_begin..]
+                .lines()
+                .next()
+                .unwrap_or(&input[line_begin..])
+                .trim_end();
+
+            // The (1-indexed) column number is the offset of our substring into that line
+            let column_number = line.offset(substring) + 1;
+
+            match kind {
+                VerboseErrorKind::Char(c) => {
+                    if let Some(actual) = substring.chars().next() {
+                        write!(
+                            &mut result,
+                            "{i}: at line {line_number}:\n\
+               {line}\n\
+               {caret:>column$}\n\
+               expected '{expected}', found {actual}\n\n",
+                            i = i,
+                            line_number = line_number,
+                            line = line,
+                            caret = '^',
+                            column = column_number,
+                            expected = c,
+                            actual = actual,
+                        )
+                    } else {
+                        write!(
+                            &mut result,
+                            "{i}: at line {line_number}:\n\
+               {line}\n\
+               {caret:>column$}\n\
+               expected '{expected}', got end of input\n\n",
+                            i = i,
+                            line_number = line_number,
+                            line = line,
+                            caret = '^',
+                            column = column_number,
+                            expected = c,
+                        )
+                    }
+                }
+                VerboseErrorKind::Context(s) => write!(
+                    &mut result,
+                    "{i}: at line {line_number}, in {context}:\n\
+             {line}\n\
+             {caret:>column$}\n\n",
+                    i = i,
+                    line_number = line_number,
+                    context = s,
+                    line = line,
+                    caret = '^',
+                    column = column_number,
+                ),
+                VerboseErrorKind::Nom(e) => write!(
+                    &mut result,
+                    "{i}: at line {line_number}, in {nom_err:?}:\n\
+             {line}\n\
+             {caret:>column$}\n\n",
+                    i = i,
+                    line_number = line_number,
+                    nom_err = e,
+                    line = line,
+                    caret = '^',
+                    column = column_number,
+                ),
+            }
+        }
+        // Because `write!` to a `String` is infallible, this `unwrap` is fine.
+        .unwrap();
+    }
+
+    result
 }
